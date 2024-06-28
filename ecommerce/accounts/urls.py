99572aab547973bd2e809c20c1447991ead0b58f
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-06-29 05:56:31 +0600

    auth complate

--- ecommerce/accounts/urls.py
+++ ecommerce/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.user_login, name='user_login'),
+    path('logout/', views.logout, name='logout'),
+    path('verification/<int:verification_code>/', views.verification, name='verification'),
 ]
--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 import random
 from .models import Verify
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,logout as user_Logout,login
 
 # Create your views here.
 def user_login(request):
@@ -20,8 +23,36 @@ def user_login(request):
                 user.set_password(password)
                 user.save()
 
-                otp = random.randint(000000,999999)
-
-
+                verification_code = random.randint(0000,9999)
+                prof = Verify.objects.create(user=user,verification_code=verification_code)
+                prof.save()
                 
+                Subject = 'Accounts Verification'
+                Body = f'Your Verification code is : http://127.0.0.1:8000/accounts/verification/{verification_code}/'
+                recipient_list = [email]
+                Email_from = settings.EMAIL_HOST_USER
+                send_mail(Subject, Body,Email_from,recipient_list)
+                return redirect('user_login')      
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request,user)
+            return redirect('home') 
+
     return render(request, 'login_register.html')
+
+def verification(request,verification_code):
+    Verified = Verify.objects.get(verification_code=verification_code)
+    Verified.is_verified = True
+    Verified.save()
+    return redirect('user_login')
+
+
+
+def logout(request):
+    user_Logout(request)
+    return redirect('user_login')
