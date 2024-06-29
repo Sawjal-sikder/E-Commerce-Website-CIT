861e410f01724b018dcbefdad3573fbd6e6159b2
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-06-29 09:55:46 +0600

    login requer

--- ecommerce/dashboard/views.py
+++ ecommerce/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='user_login')
+def dashboard(request):
+    user = request.user
+    return render(request,'dashboard.html',locals())
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    # path('dashboard/', include('dashboard.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
 
 
