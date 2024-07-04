180b9fdfb2e30d25340db5162c9def973dfcfb93
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-07-04 13:00:05 +0600

    search

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.product, name='product'),
-   
+    path('search/', views.serarch, name='search'),
+    path('single_product/<slug:slug>/', views.single_product, name='single_product'),
+
 ]
 
 
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.db.models import Q
 # Create your views here.
 def product(request):
-    pass
+    products = Product.objects.all()
+    return render(request, 'products.html',
+                  {
+                      'products':products
+                  })
+
+def single_product(request, slug):
+    single_product = Product.objects.get(slug=slug)
+    return render(request,'single_product.html',
+                  {
+                      'single_product':single_product
+                  })
+def serarch(request):
+    if request.method == 'GET':
+        query_search = request.GET.get('search')
+        print(query_search)
+        if query_search:
+            products = Product.objects.filter(Q(name__icontains=query_search) | Q(sub_category__icontains=query_search))
+    return render(request, 'products.html',
+                  {
+                      'products':products
+                  })
+
