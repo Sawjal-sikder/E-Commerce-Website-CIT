b4b2bd7adc9263fd8bcfdfeada8b587634860599
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-07-05 17:15:59 +0600

    related products set

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product, name='product'),
+    path('categories/<slug:slug>/', views.categories, name='categories'),
     path('search/', views.serarch, name='search'),
     path('single_product/<slug:slug>/', views.single_product, name='single_product'),
 
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, SubCategory
 from django.db.models import Q
 # Create your views here.
+
+def categories(request, slug):
+    catagories_products = Product.objects.filter(sub_category__slug=slug)
+    return render(request, 'category_products.html',{'catagories_products':catagories_products})
 def product(request):
     products = Product.objects.all()
-    return render(request, 'products.html',
-                  {
-                      'products':products
-                  })
+    return render(request, 'products.html',{'products':products})
 
 def single_product(request, slug):
     single_product = Product.objects.get(slug=slug)
+    subcategories = single_product.sub_category.category.subcategories.all()
+    related_products = single_product.sub_category.products.exclude(id=single_product.id)
     return render(request,'single_product.html',
                   {
-                      'single_product':single_product
+                      'single_product':single_product,
+                      'subcategories':subcategories,
+                      'related_products':related_products,
+
                   })
+
+
+
 def serarch(request):
     if request.method == 'GET':
         query_search = request.GET.get('search')
         print(query_search)
         if query_search:
-            products = Product.objects.filter(Q(name__icontains=query_search) | Q(sub_category__icontains=query_search))
+            products = Product.objects.filter(name__icontains=query_search)
     return render(request, 'products.html',
                   {
                       'products':products
