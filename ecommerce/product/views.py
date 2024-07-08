0300a24fc310ce1e3dc586ed4ee3f2012612d4c6
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-07-08 15:48:59 +0600

    category recirect

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Product, SubCategory
+from .models import Product, SubCategory, Category
 from django.db.models import Q
 # Create your views here.
 
 def categories(request, slug):
-    catagories_products = Product.objects.filter(sub_category__slug=slug)
+    sub_category = SubCategory.objects.get(slug=slug)
+    catagories_products = Product.objects.filter(sub_category=sub_category)
+
     return render(request, 'category_products.html',{'catagories_products':catagories_products})
 def product(request):
     products = Product.objects.all()
@@ -12,13 +14,15 @@ def product(request):
 
 def single_product(request, slug):
     single_product = Product.objects.get(slug=slug)
-    subcategories = single_product.sub_category.category.subcategories.all()
-    related_products = single_product.sub_category.products.exclude(id=single_product.id)
+    subcategories = SubCategory.objects.filter(category=single_product.category)
+    related_products = Product.objects.filter(sub_category=single_product.sub_category)
+    new_products = Product.objects.all()[:3]
     return render(request,'single_product.html',
                   {
                       'single_product':single_product,
                       'subcategories':subcategories,
                       'related_products':related_products,
+                      'new_products':new_products,
 
                   })
 
